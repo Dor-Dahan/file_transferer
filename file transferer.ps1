e685e84a5857ea4a658272265b3fa79e68940a0b
dor <32441786+dooood5@users.noreply.github.com>
2022-08-01 12:14:14 +0300

    file transferer
    needed extra file that called iplist.txt that contain hostname/ip list

---
+++ file transferer.ps1
@@ -0,0 +1,9 @@
+﻿$linkname = Read-Host "enter Shortcut name "
+$cpn = Get-Content .\iplist.txt
+foreach($name in $cpn){
+        Copy-Item -path ".\XXfilenameXX" -Destination "\\$name\c$\Windows" -Force -Recurse -ErrorAction SilentlyContinue
+    $users = Get-ChildItem -Directory \\$name\c$\users
+    foreach($user in $users){
+        Copy-Item ".\$linkname.lnk" -Destination "\\$name\c$\Users\$user\Desktop" -ErrorAction SilentlyContinue
+        }} 
+Copy-Item ".\$linkname.lnk" -Destination "\\$name\c$\Users\Default\Desktop" -ErrorAction SilentlyContinue
